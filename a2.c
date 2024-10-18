ba4175010edae18f562aa2c2fecc7a957c5b4425
DomTorres <neildominictorres@gmail.com>
2024-10-18 14:04:21 -0700

    added error handling for student fields

--- a2.c
+++ a2.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX_STUDENTS 50
+#define MAX_LINE_LEN 50
 #define MAX_NAME_LEN 10
 
 typedef struct 
@@ -16,50 +18,193 @@ typedef struct
     int TOEFL;
 } student;
 
+int loadStudents(FILE *inputFilePointer, FILE *outputFilePointer, int option, student *students, int *studentCount);
 student *sortStudents(student *student, int numberOfStudents);
 student *mergeStudents(student *leftStudents, int numberOfLeftStudents, student *rightStudents, int numberOfRightStudents);
 student *studentsCopy(student *students, int numberOfStudents);
 int compareStudents(student a, student b);
 int compareNames(char *a, char *b);
 void printStudent(student s);
+int monthStringToInt(char *month);
+void writeToOutput(FILE *outputFilePointer, student *students, int studentCount);
 
-int main()
+int main(int argc, char *argv[])
 {
-    int numberOfStudents = 5;
+    // validate arg counter
+    if(argc != 4)
+    {
+        printf("Error: Usage must be ./a2 <input file> <output file> <option>\n");
+        return 1;
+    }
 
-    student *students = (student *) malloc(numberOfStudents * sizeof(student));
+    // validate option
+    int option = atoi(argv[3]);
+    if(!(option == 1 || option == 2 || option == 3))
+    {
+        printf("Error: Invalid option.\n");
+        return 1;
+    }
 
-    student mary = {1990, 2, 2, "Mary", "Jackson", 4.0, 'I', 60};
-    student jack = {1990, 2, 3, "Jack", "He", 2.45, 'D'};
-    student mike = {1980, 9, 2, "Mike", "Johnson", 3.125, 'D'};
-    student jane = {1970, 3, 2, "Jane", "Zhang", 3.8, 'I', 120};
-    student bob = {1990, 2, 3, "Bob", "He", 2.45, 'D'};
+    // open input file
+    FILE *inputFilePointer = fopen(argv[1], "r");
+    if(!inputFilePointer)
+    {
+        printf("Error: Input file not found.\n");
+        return 1;
+    }
 
-    students[0] = mary;
-    students[1] = jack;
-    students[2] = mike;
-    students[3] = jane;
-    students[4] = bob;
+    // open output file
+    FILE *outputFilePointer = fopen(argv[2], "w");
+    if(!outputFilePointer)
+    {
+        printf("Error: Output file cannot be created.\n");
+        return 1;
+    }
+
+    // load students from input file
+    student *students = (student *) malloc(MAX_STUDENTS * sizeof(student));
+    if(!students)
+    {
+        printf("Error: Memory allocation error.\n");
+        return 1;
+    }
+
+    int studentCount = 0;
+
+    if(loadStudents(inputFilePointer, outputFilePointer, option, students, &studentCount) != 0)
+    {
+        return 1;
+    }
     
-    printf("\nUNSORTED:\n");
-    for(int i = 0; i < numberOfStudents; i++)
-    {
-        printStudent(students[i]);
-    }
+    // printf("\nUNSORTED:\n");
+    // for(int i = 0; i < studentCount; i++)
+    // {
+    //     printStudent(students[i]);
+    // }
 
-    student *sortedStudents = sortStudents(students, numberOfStudents);
+    // sort students
+    student *sortedStudents = sortStudents(students, studentCount);
 
-    printf("\nSORTED:\n");
-    for(int i = 0; i < numberOfStudents; i++)
-    {
-        printStudent(sortedStudents[i]);
-    }
+    // printf("\nSORTED:\n");
+    // for(int i = 0; i < studentCount; i++)
+    // {
+    //     printStudent(sortedStudents[i]);
+    // }
+
+    writeToOutput(outputFilePointer, sortedStudents, studentCount);
 
     free(students);
     free(sortedStudents);
 
+    fclose(inputFilePointer);
+
     return 0;
 } 
+
+int loadStudents(FILE *inputFilePointer, FILE *outputFilePointer, int option, student *students, int *studentCount)
+{
+    // printf("STUDENT COUNT: %d\n", *studentCount);
+
+    char line[MAX_LINE_LEN];
+
+    while(fgets(line, MAX_LINE_LEN, inputFilePointer))
+    {
+        // parse line, store in variables
+        char firstName[MAX_NAME_LEN];
+        char lastName[MAX_NAME_LEN];
+
+        char month[10]; // extra buffer
+        int day = -1;
+        int year = -1;
+        
+        double GPA = -1;
+        char immigration = ' ';
+        int TOEFL = -1;
+
+        sscanf(line, "%s %s %3s-%d-%d %lf %c %d",
+            firstName,
+            lastName,
+            month,
+            &day,
+            &year,
+            &GPA,
+            &immigration,
+            &TOEFL);
+
+        // if we don't need to save this student, then don't bother creating a student
+        int saveDomestic = immigration == 'D' && (option == 1 || option == 3);
+        int saveInternational = immigration == 'I' && (option == 2 || option == 3);
+
+        if(!(saveDomestic || saveInternational))
+        {
+            continue;
+        }
+
+        int monthInt = monthStringToInt(month);
+
+        // validate fields
+        if(immigration == 'I' && !(0 <= TOEFL && TOEFL <= 120))
+        {
+            fprintf(outputFilePointer, "Error: Invalid TOEFL.\n");
+            return 1;
+        }
+
+        if(immigration == 'D' && TOEFL != -1)
+        {
+            fprintf(outputFilePointer, "Error: Domestic student must not have a TOEFL field.\n");
+            return 1;
+        }
+
+        if(!(immigration == 'I' || immigration == 'D'))
+        {
+            fprintf(outputFilePointer, "Error: Invalid immigration status.\n");
+            return 1;
+        }
+
+        if(!(0.0 <= GPA && GPA <= 4.3))
+        {
+            fprintf(outputFilePointer, "Error: Invalid GPA.\n");
+            return 1;
+        }
+
+        if(monthInt == -1)
+        {
+            fprintf(outputFilePointer, "Invalid month.\n");
+            return 1;
+        }
+
+        if(!(1950 <= year && year <= 2010))
+        {
+            fprintf(outputFilePointer, "Error: Invalid year.\n");
+            return 1;
+        }
+
+        // TO DO (?): validate day 
+
+        // create student and populate fields
+        student this;
+
+        this.year = year;
+        this.month = monthInt;
+        this.day = day;
+        strcpy(this.firstName, firstName);
+        strcpy(this.lastName, lastName);
+        this.GPA = GPA;
+        this.immigration = immigration;
+        if(immigration == 'I') { this.TOEFL = TOEFL; }
+
+        // save student
+        students[(*studentCount)++] = this;
+
+        if(*studentCount >= MAX_STUDENTS)
+        {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
 
 student *sortStudents(student *students, int numberOfStudents)
 {
@@ -163,6 +308,7 @@ int compareStudents(student a, student b)
     }
     else if(a.immigration == 'I' && b.immigration == 'D')
     {
+        // different immigration statuses
         return 1;
     }
     else if(a.immigration == 'I' && b.immigration == 'I' && a.TOEFL != b.TOEFL) 
@@ -174,6 +320,7 @@ int compareStudents(student a, student b)
 } 
 
 // Positive if a comes alphabetically after b, negative if a alphabetically comes before b, 0 if a is the same as b
+// TO DO: NAMES MUST BE CASE SENSITIVE (not yet handled)
 int compareNames(char *a, char *b)
 {
     for(int i = 0; i < MAX_NAME_LEN; i++)
@@ -191,11 +338,53 @@ void printStudent(student s)
 {
     char *months[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
 
-    printf("%s %s %s-%d-%d %f %c %d\n",
+    printf("%s %s %s-%d-%d %f %c",
         s.firstName, s.lastName,
         months[s.month - 1], s.day, s.year,
         s.GPA,
-        s.immigration,
-        s.immigration == 'I' ? s.TOEFL : 0
+        s.immigration
     );
+
+    if(s.immigration == 'I') { printf(" %d", s.TOEFL); }
+
+    printf("\n");
 }
+
+int monthStringToInt(char *month)
+{
+    if(strcmp(month, "Jan") == 0) { return 1; }
+    if(strcmp(month, "Feb") == 0) { return 2; }
+    if(strcmp(month, "Mar") == 0) { return 3; }
+    if(strcmp(month, "Apr") == 0) { return 4; }
+    if(strcmp(month, "May") == 0) { return 5; }
+    if(strcmp(month, "Jun") == 0) { return 6; }
+    if(strcmp(month, "Jul") == 0) { return 7; }
+    if(strcmp(month, "Aug") == 0) { return 8; }
+    if(strcmp(month, "Sep") == 0) { return 9; }
+    if(strcmp(month, "Oct") == 0) { return 10; }
+    if(strcmp(month, "Nov") == 0) { return 11; }
+    if(strcmp(month, "Dec") == 0) { return 12; }
+    return -1;
+}
+
+void writeToOutput(FILE *outputFilePointer, student *students, int studentCount)
+{
+    char *months[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+
+    for(int i = 0; i < studentCount; i++)
+    {
+        student s = students[i];
+
+        fprintf(outputFilePointer, "%s %s %s-%d-%d %f %c",
+            s.firstName, s.lastName,
+            months[s.month - 1], s.day, s.year,
+            s.GPA,
+            s.immigration
+        );
+
+        if(s.immigration == 'I') { fprintf(outputFilePointer ," %d", s.TOEFL); }
+
+        fprintf(outputFilePointer, "\n");
+    }
+}
+
--- test.c
+++ test.c
@@ -3,9 +3,22 @@
 
 int main()
 {
-    char *pmessage = "now is the time"; /* a pointer */
+    // char *pmessage = "now is the time"; /* a pointer */
 
-    *(pmessage + 3) = 'A';
+    // *(pmessage + 3) = 'A';
 
-    printf("%s\n", pmessage);
+    // printf("%s\n", pmessage);
+
+    // int *arr1 = (int *) malloc(90 * sizeof(int));
+
+    // for(int i = 0; i < 90; i++)
+    // {
+    //     printf("%d\n", arr1[i]);
+    // }
+
+    int a = 320;
+    printf("%c\n", 320);
+
+//     int arr2[5];
+//     printf("%d\n", arr2[3]);
 }
